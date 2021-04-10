e9ec2dabc3673cb16c320ef47efd1cd60807b1c0
phoenix34 <phoenix34@protonmail.com>
2021-04-11 00:15:38 +0200

    add noloop option and proper help

--- tools/twig-convert.js
+++ tools/twig-convert.js
@@ -7,13 +7,18 @@ const writeFile = util.promisify(fs.writeFile);
 
 (async () => {
 
-  const [, , file, ...mappingRaw] = process.argv;
+  const [, , file, ...args] = process.argv;
 
   const map = new Map();
-  for (const raw of mappingRaw) {
-    const [key, val] = raw.split(':');
-    map.set(new RegExp(key, 'g'), val);
-    map.set(new RegExp(`__REMAIN_${val}__`, 'g'), key);
+  let allowLoop = true;
+  for (const raw of args) {
+    if ('--noloop' === raw) {
+      allowLoop = false;
+    } else {
+      const [key, val] = raw.split(':');
+      map.set(new RegExp(key, 'g'), val);
+      map.set(new RegExp(`__REMAIN_${val}__`, 'g'), key);
+    }
   }
 
   let data = (await readFile(file)).toString()
@@ -23,20 +28,23 @@ const writeFile = util.promisify(fs.writeFile);
     .replace(/<!-- ENDIF -->/g, '{% endif %}')
     .replace(/<!-- INCLUDE (.+?) -->/g, "{{ include('$1') }}")
     .replace(/<!-- EVENT (.+?) -->/g, '{% EVENT $1 %}')
-    // Basically the separation of the BEGIN/BEGINELSE/END loop of
-    // the phpBB template engine to a twig way of doing loops. I
-    // don't want to go into details, because... whatever!
-    .replace(/<!-- BEGIN (.+?) -->(([\s\n\r\t]|.)+?)(<!-- BEGINELSE -->(([\s\n\r\t]|.)+?))?<!-- END .+? -->/g, (_, list, content, _2, _3, suffixContent) => {
-      return suffixContent ?
-        `{% if __REMAIN_${list}__|length > 0 %}{% for ${list} in __REMAIN_${list}__ %}${content}{% endfor %}{% else %}${suffixContent}{% endif %}` :
-        `{% for ${list} in __REMAIN_${list}__ %}${content}{% endfor %}`
-    })
     // replace variable output
     .replace(/\{+\s*([^{}#%]+)\s*\}+/g, '{{ $1 }}')
     // replace i18n output from changed variable, because
     // it partly works only with one {L_***} and not the
     // twig way {{ L_*** }}
     .replace(/\{+\s*(L_[^{}#% ]+)\s*\}+/g, '{$1}');
+
+  if (allowLoop) {
+    // Basically the separation of the BEGIN/BEGINELSE/END loop of
+    // the phpBB template engine to a twig way of doing loops. I
+    // don't want to go into details, because... whatever!
+    data = data.replace(/<!-- BEGIN (.+?) -->(([\s\n\r\t]|.)+?)(<!-- BEGINELSE -->(([\s\n\r\t]|.)+?))?<!-- END .+? -->/g, (_, list, content, _2, _3, suffixContent) => {
+      return suffixContent ?
+        `{% if __REMAIN_${list}__|length > 0 %}{% for ${list} in __REMAIN_${list}__ %}${content}{% endfor %}{% else %}${suffixContent}{% endif %}` :
+        `{% for ${list} in __REMAIN_${list}__ %}${content}{% endfor %}`
+    });
+  }
 
   for (const [pattern, replacement] of map.entries()) {
     data = data.replace(pattern, replacement);
