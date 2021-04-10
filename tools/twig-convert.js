911478bb9f68c02696e683d5f1ad5898d906c767
phoenix34 <phoenix34@protonmail.com>
2021-04-10 23:18:57 +0200

    add begin/beginelse/end loop to twig-convert

--- tools/twig-convert.js
+++ tools/twig-convert.js
@@ -7,18 +7,42 @@ const writeFile = util.promisify(fs.writeFile);
 
 (async () => {
 
-  const [, , file] = process.argv;
-  const data = (await readFile(file)).toString();
+  const [, , file, ...mappingRaw] = process.argv;
 
-  await writeFile(file, data
+  const map = new Map();
+  for (const raw of mappingRaw) {
+    const [key, val] = raw.split(':');
+    map.set(new RegExp(key, 'g'), val);
+    map.set(new RegExp(`__REMAIN_${val}__`, 'g'), key);
+  }
+
+  let data = (await readFile(file)).toString()
     .replace(/<!-- IF (.+?) -->/g, '{% if $1 %}')
     .replace(/<!-- ELSE ?IF (.+?) -->/g, '{% elseif $1 %}')
     .replace(/<!-- ELSE -->/g, '{% else %}')
     .replace(/<!-- ENDIF -->/g, '{% endif %}')
     .replace(/<!-- INCLUDE (.+?) -->/g, "{{ include('$1') }}")
     .replace(/<!-- EVENT (.+?) -->/g, '{% EVENT $1 %}')
+    // Basically the separation of the BEGIN/BEGINELSE/END loop of
+    // the phpBB template engine to a twig way of doing loops. I
+    // don't want to go into details, because... whatever!
+    .replace(/<!-- BEGIN (.+?) -->(([\s\n\r\t]|.)+?)(<!-- BEGINELSE -->(([\s\n\r\t]|.)+?))?<!-- END .+? -->/g, (_, list, content, _2, _3, suffixContent) => {
+      return suffixContent ?
+        `{% if __REMAIN_${list}__|length > 0 %}{% for ${list} in __REMAIN_${list}__ %}${content}{% endfor %}{% else %}${suffixContent}{% endif %}` :
+        `{% for ${list} in __REMAIN_${list}__ %}${content}{% endfor %}`
+    })
+    // replace variable output
     .replace(/\{+\s*([^{}#%]+)\s*\}+/g, '{{ $1 }}')
-    .replace(/\{+\s*(L_[^{}#% ]+)\s*\}+/g, '{$1}')
-  );
+    // replace i18n output from changed variable, because
+    // it partly works only with one {L_***} and not the
+    // twig way {{ L_*** }}
+    .replace(/\{+\s*(L_[^{}#% ]+)\s*\}+/g, '{$1}');
+
+  for (const [pattern, replacement] of map.entries()) {
+    data = data.replace(pattern, replacement);
+    console.log(pattern, '->', replacement);
+  }
+
+  await writeFile(file, data);
 
 })().catch(console.error);
