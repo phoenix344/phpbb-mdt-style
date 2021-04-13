4b58796ea9dd8a3c262ab9c81f4725cba482b74f
phoenix34 <phoenix34@protonmail.com>
2021-04-14 01:58:02 +0200

    refactoring #2

---
+++ theme/assets/_drawer.js
@@ -0,0 +1,37 @@
+
+// Add transition to drawer when page is actually loaded.
+window.addEventListener('load', () => {
+  const drawer = document.querySelector('.mdl-layout__drawer');
+  drawer.classList.add('pbb-drawer-delayed-transition');
+});
+
+// On resize the drawer appears to rerender and
+// interrupts the view. So let's take care of it!
+
+let rtime;
+let timeout = false;
+const delta = 200;
+
+// On the start of the resize event the transition
+// class will be removed.
+window.addEventListener('resize', () => {
+  rtime = new Date();
+  if (timeout === false) {
+    timeout = true;
+    const drawer = document.querySelector('.mdl-layout__drawer');
+    drawer.classList.remove('pbb-drawer-delayed-transition');
+    setTimeout(resizeend, delta);
+  }
+});
+
+// After a delay of 200ms the transition class will be
+// added again to the drawer element.
+function resizeend() {
+  if (new Date() - rtime < delta) {
+    setTimeout(resizeend, delta);
+  } else {
+    timeout = false;
+    const drawer = document.querySelector('.mdl-layout__drawer');
+    drawer.classList.add('pbb-drawer-delayed-transition');
+  }
+}
--- theme/assets/main.js
+++ theme/assets/main.js
@@ -1,10 +1,5 @@
 import "./_colormode.js";
+import "./_drawer.js";
 import "./_profile.js";
 import "./_toolbar.js";
 import "./_poll.js";
-
-// Add transition to drawer when page is actually loaded.
-window.addEventListener('load', () => {
-  const drawer = document.querySelector('.mdl-layout__drawer');
-  drawer.classList.add('pbb-drawer-delayed-transition');
-});
